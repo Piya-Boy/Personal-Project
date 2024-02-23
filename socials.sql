a07ef04a6fcabf6621641f7d36ec05e93db3f1ab
Piya-Boy <87295280+Piya-Boy@users.noreply.github.com>
2024-02-23 11:43:20 +0700

    update package

---
+++ socials.sql
@@ -0,0 +1,114 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MYSQL80
+ Source Server Type    : MySQL
+ Source Server Version : 80035 (8.0.35)
+ Source Host           : localhost:3360
+ Source Schema         : socials
+
+ Target Server Type    : MySQL
+ Target Server Version : 80035 (8.0.35)
+ File Encoding         : 65001
+
+ Date: 23/02/2024 11:09:36
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `desc` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `usersid` int NOT NULL,
+  `postid` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `Comments_usersid_fkey`(`usersid` ASC) USING BTREE,
+  INDEX `Comments_postid_fkey`(`postid` ASC) USING BTREE,
+  CONSTRAINT `Comments_postid_fkey` FOREIGN KEY (`postid`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Comments_usersid_fkey` FOREIGN KEY (`usersid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for likes
+-- ----------------------------
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userid` int NOT NULL,
+  `postid` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `Likes_userid_fkey`(`userid` ASC) USING BTREE,
+  INDEX `Likes_postid_fkey`(`postid` ASC) USING BTREE,
+  CONSTRAINT `Likes_postid_fkey` FOREIGN KEY (`postid`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Likes_userid_fkey` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 242 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for posts
+-- ----------------------------
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `desc` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `img` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `usersid` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `Posts_usersid_fkey`(`usersid` ASC) USING BTREE,
+  CONSTRAINT `Posts_usersid_fkey` FOREIGN KEY (`usersid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 64 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for relationships
+-- ----------------------------
+DROP TABLE IF EXISTS `relationships`;
+CREATE TABLE `relationships`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `followerUserid` int NOT NULL,
+  `followedUserid` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `Relationships_followerUserid_fkey`(`followerUserid` ASC) USING BTREE,
+  INDEX `Relationships_followedUserid_fkey`(`followedUserid` ASC) USING BTREE,
+  CONSTRAINT `Relationships_followedUserid_fkey` FOREIGN KEY (`followedUserid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Relationships_followerUserid_fkey` FOREIGN KEY (`followerUserid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 64 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for stories
+-- ----------------------------
+DROP TABLE IF EXISTS `stories`;
+CREATE TABLE `stories`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `img` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `userid` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `Stories_userid_fkey`(`userid` ASC) USING BTREE,
+  CONSTRAINT `Stories_userid_fkey` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `coverPic` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `profilePic` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `website` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `Users_username_key`(`username` ASC) USING BTREE,
+  UNIQUE INDEX `Users_email_key`(`email` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
